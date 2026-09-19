57fd014e0e42d346958f8d5bf48587151545af8b
thanhnha01 <96094930+thanhnha01@users.noreply.github.com>
2026-09-19 15:27:49 +0700

    fix: import weight for production detail layout

--- app/src/main/java/com/thanhnha/universalacremote/ProductionDetailsScreen.kt
+++ app/src/main/java/com/thanhnha/universalacremote/ProductionDetailsScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.weight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AcUnit
 import androidx.compose.material.icons.filled.Air
