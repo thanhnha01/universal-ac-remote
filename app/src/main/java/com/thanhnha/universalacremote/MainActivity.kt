fc203dc13249aaec90a5904ef03c2e107f36685b
thanhnha01 <96094930+thanhnha01@users.noreply.github.com>
2026-09-20 11:33:51 +0700

    refactor(app): use a single shared MainActivity

---
+++ app/src/main/java/com/thanhnha/universalacremote/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.thanhnha.universalacremote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.thanhnha.universalacremote.ir.readIrHardwareDiagnostics
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val diagnostics = readIrHardwareDiagnostics(this)
+        setContent {
+            RemoteApp(diagnostics)
+        }
+    }
+}
